f5822aee835488bca5609707f37caf0da2feeb5a
Aubhro <aubhrosengupta@gmail.com>
2017-04-15 20:11:18 -0700

    Add preprocessing methods

--- web/cluster_acc.py
+++ web/cluster_acc.py
@@ -1,2 +1,39 @@
-import sklearn
+import os
+import numpy as np
 
+STREAM_PATH = "stream"
+PREVIOUS_ACTION_PATH = "action"
+
+ZERO_ACC = np.array([0, 0, 0])
+STOP_ERROR_DISTANCE_CONSTANT = 1
+
+TRIGGER_ACTION = np.array([0, 2])
+
+def save_coordinate_to_npy(x, y, z):
+    if os.path.exists(STREAM_PATH):
+        stream = np.load(STREAM_PATH)
+    else:
+        stream = np.array([[x, y, z]])
+    np.save(STREAM_PATH, stream)
+
+def stopped(acc3D):
+    return np.linalg.norm(acc3D - ZERO_ACC) <= STOP_ERROR_DISTANCE_CONSTANT
+
+def load_coordinate_stream(x, y, z):
+    stream = np.load(STREAM_PATH)
+
+    if os.path.exists(PREVIOUS_ACTION_PATH):
+        previous_action = np.load(PREVIOUS_ACTION_PATH)
+
+        if stopped(np.array([x, y, z])):
+            convert_points_to_vector(stream)
+
+    np.save(PREVIOUS_ACTION_PATH, classify_action(stream))
+    np.save(STREAM_PATH, np.array([]))
+
+def convert_points_to_vector(stream):
+    pass
+
+# TODO Vikranth
+def classify_action(vector):
+    return np.array([0])
--- web/website/views.py
+++ web/website/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 
+from web.cluster_acc import save_coordinate_to_npy, load_coordinate_stream
+
 # Create your views here.
 def index(request):
     x = request.POST['x']
     y = request.POST['y']
     z = request.POST['z']
+
+    save_coordinate_to_npy(x, y, z)
+    load_coordinate_stream()
 
     return render(request, 'index.html', {
             'videos': 'hello',
