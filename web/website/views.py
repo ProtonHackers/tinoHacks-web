2d0abf157f3989068d6000979f9b7d5f5d44b196
Vikranth Srivatsa <rama22446@gmail.com>
2017-04-15 18:00:13 -0700

    Post Request

--- web/website/views.py
+++ web/website/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
+    x = request.POST['x']
+    y = request.POST['y']
+    z = request.POST['z']
+
     return render(request, 'index.html', {
             'videos': 'hello',
         })
